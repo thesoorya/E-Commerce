167c0c8bfbdf7f365e0cb3eaa8f67984f65abcad
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-04-29 13:03:37 +0530

    Pages Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Collections from './pages/Collections/Collections'
+import Login from './pages/Login/Login'
+import Signup from './pages/SignUp/Signup'
+import Cart from './pages/Cart/Cart'
+import Product from './pages/Product/Product'
 import './App.css'
 
 function App() {
@@ -14,6 +18,10 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/collections' element={<Collections />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Signup />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/product' element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
